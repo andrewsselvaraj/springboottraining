c292af36d4d22405a1f03dd951ea9e39f1136fbd
Rajesh <wraejesh@gmail.com>
2022-08-14 19:38:06 +0530

    initial commit

---
+++ src/main/java/com/example/demo/SpringppocApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringppocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringppocApplication.class, args);
+	}
+
+}
